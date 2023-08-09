2fdf0737f0ae27df153f31a37afb19bb4ce7e810
Franky101 <franco.becerra@mi.unc.edu.ar>
2023-08-09 11:55:49 -0300

    Initial Commit

---
+++ src/main/java/calculator/main.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("The result is negative, be careful!");
+        System.out.println("The result is a large number.")
+    }
+}
